f4907ca53e6754365cdb7ecbed6d39beb9ec4f73
Vincent Guidon <vinc.guidon@gmail.com>
2017-03-20 12:18:11 +0800

    Add color pickers

--- app/src/main/java/com/andpad/andpad/MainActivity.java
+++ app/src/main/java/com/andpad/andpad/MainActivity.java
@@ -1,11 +1,17 @@
 package com.andpad.andpad;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.preference.PreferenceFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements ColorPickerDialog.OnColorChangedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,16 +19,33 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
+    public void changeView(View v)
+    {
+        RelativeLayout rl = (RelativeLayout) findViewById(R.id.activity_main);
+        Drawable background = v.getBackground();
 
-    public void changeView(View view) {
+        ColorDrawable viewColor = (ColorDrawable) rl.getBackground();
+        int colorId = viewColor.getColor();
+
+        new ColorPickerDialog(MainActivity.this, MainActivity.this, colorId).show();
+    }
+  /*  public void changeView(View view) {
         Intent intent =  new Intent(this, NotePad.class);
         intent.putExtra("Title", "Inside");
         startActivity(intent);
-    }
+
+        new ColorPickerDialog(FingerPaintActivity.this, FingerPaintActivity.this, mPaint.getColor()).show();
+    }*/
 
     public void listView(View view) {
         Intent intent = new Intent(this, ListViewAndroidExampleActivity.class);
 
         startActivity(intent);
+    }
+
+    @Override
+    public void colorChanged(int color) {
+        RelativeLayout rl = (RelativeLayout) findViewById(R.id.activity_main);
+        rl.setBackgroundColor(color);
     }
 }
